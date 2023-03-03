241c0824b16f186d7f1db3eb8a5e81a4015518d5
Sümeyra Duran <smyrduran3535@gmail.com>
2023-03-04 00:18:58 +0300

    sd

---
+++ src/main/java/Ogrenci.java
@@ -0,0 +1,38 @@
+public class Ogrenci extends Kisi{//
+    private String ogrenciNo;
+    private String sinif;
+    //cons secerken extend yaptıgımız ilk cıkan parametreli olanı secicez
+    //tekrardan soracak bu classdakilerde yapmak istediklerimizi
+
+    public Ogrenci(String adSoyad, String kimlikNo, int yas, String ogrenciNo, String sinif) {
+        super(adSoyad, kimlikNo, yas);//bizi Kisi classındaki const.goturur
+        this.ogrenciNo = ogrenciNo;
+        this.sinif = sinif;
+    }
+    public Ogrenci(){//ekle hep sıkıntı cıkarmasın
+    }
+
+    public String getOgrenciNo() {
+        return ogrenciNo;
+    }
+
+    public void setOgrenciNo(String ogrenciNo) {
+        this.ogrenciNo = ogrenciNo;
+    }
+
+    public String getSinif() {
+        return sinif;
+    }
+
+    public void setSinif(String sinif) {
+        this.sinif = sinif;
+    }
+
+    @Override
+    public String toString() {
+        return "Ogrenci{" +
+                "ogrenciNo='" + ogrenciNo + '\'' +
+                ", sinif='" + sinif + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Islemler.girisPaneli();//class adıyla da cagirilir importta edebiliriz.run edince once bu calısır
+        // Islemler.girisPaneli(); boyle de cagırabiliriz
+
+
+    }
+
+}
